e8004988bc3b3f6ef2e265fa0ff75de8720dd516
Sponsi1998 <sponsi1998@hotmail.com>
2021-09-10 19:41:48 +0200

    Lets Try

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, ViewChild, ElementRef, Directive } from '@angular/core';
+import { BackgroundMusicService } from '../background-music.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+
+export class HomeComponent implements OnInit {
+
+  constructor(private backgroundMusicService: BackgroundMusicService) {
+    this.backgroundMusicService.setMusicSrc("../../assets/sounds/Homesound.mp3");
+  }
+
+  ngOnInit() {
+  }
+}
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BackgroundMusicService } from '../background-music.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  value: number = 50;
+  playPauseButton: string = "";
+
+  constructor(private http: HttpClient, private router: Router, private backgroundMusicService: BackgroundMusicService) { }
+
+  ngOnInit() {
+    if(this.backgroundMusicService.isMusicPlaying){
+      this.playPauseButton = "green";
+    }
+    else{
+      this.playPauseButton = "red";
+    }
+  }
+
+  get volumen(){
+    return this.value;
+  }
+
+  set volumen(num:number){
+    this.value = num;
+    this.backgroundMusicService.setMusicVol(num);
+  }
+
+  toggleMusic(){
+    this.backgroundMusicService.toggleMusic();
+    if(this.backgroundMusicService.isMusicPlaying){
+      this.playPauseButton = "green";
+    }
+    else{
+      this.playPauseButton = "red";
+    }
+  }
+}
